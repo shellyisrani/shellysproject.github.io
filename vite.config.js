b67323eba4b0e19d9482887df970910dc53c7214
shelly-nabc <shailajaisrani@yahoo.com>
2026-03-03 21:38:26 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import path from "path"
+import { fileURLToPath } from "url"
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   logLevel: "error",
   plugins: [react()],
   base: '/', // Change this to '/your-repo-name/' if deploying to a project page (not user.github.io)
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   build: {
     outDir: 'dist',
   },
