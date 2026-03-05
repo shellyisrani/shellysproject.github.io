b7ac656c9a1ec1fd0ed4a07ffa1ca2b2dec06ebc
shelly-nabc <shailajaisrani@yahoo.com>
2026-03-05 13:41:51 -0500

    trying to update image position again

--- src/components/wedding/DetailsSection.jsx
+++ src/components/wedding/DetailsSection.jsx
@@ -98,12 +98,16 @@ export default function DetailsSection() {
         </div>
 
         {/* Right - Image */}
-        <div className="relative min-h-[50vh] lg:min-h-full order-1 lg:order-2">
+        <div className="relative min-h-[50vh] lg:min-h-full order-1 lg:order-2 overflow-hidden">
           <img
             src="/images/when_where.jpg"
             alt="Wedding venue"
-            className="absolute inset-0 w-full h-full object-cover"
-            style={{ objectPosition: 'center bottom' }}
+            className="w-full h-full object-cover"
+            style={{ 
+              objectPosition: 'center 100%',
+              height: '100%',
+              width: '100%'
+            }}
           />
         </div>
       </div>
