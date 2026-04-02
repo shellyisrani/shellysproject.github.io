43fcabd712ab1b60fe5a8130152dc58322d7e586
shelly-nabc <shailajaisrani@yahoo.com>
2026-04-01 22:57:35 -0400

    update vars

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.https://wfenclffcqyeqbujfyas.supabase.co
-const supabaseAnonKey = import.meta.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndmZW5jbGZmY3F5ZXFidWpmeWFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI3NTk4NjYsImV4cCI6MjA4ODMzNTg2Nn0.JDwWPHQ28Wa2n04XUoNIGVMysNKf30ZRGhmg2a-BeT4
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
